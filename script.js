2ea426a299658900340419627538efeccaf749dc
Jean Viegas <jeanmelg@gmail.com>
2021-11-04 23:19:44 -0300

    finaliza requisito bonus

--- script.js
+++ script.js
@@ -3,6 +3,13 @@ const memeInsert = document.getElementById('meme-insert');
 const memeImageContainer = document.getElementById('meme-image-container');
 const memeText = document.getElementById('meme-text');
 const memeImage = document.createElement('img');
+const btFire = document.getElementById('fire');
+const btWater = document.getElementById('water');
+const btEarth = document.getElementById('earth');
+const meme1 = document.getElementById('meme-1');
+const meme2 = document.getElementById('meme-2');
+const meme3 = document.getElementById('meme-3');
+const meme4 = document.getElementById('meme-4');
 
 memeInsert.addEventListener('change', (e) => {
   memeImage.src = URL.createObjectURL(e.target.files[0]);
@@ -12,4 +19,40 @@ memeInsert.addEventListener('change', (e) => {
 
 textInput.addEventListener('keyup', () => {
   memeText.innerText = textInput.value;
+});
+
+btEarth.addEventListener('click', () => {
+  memeImageContainer.style.border = 'groove 6px green';
+});
+
+btFire.addEventListener('click', () => {
+  memeImageContainer.style.border = 'dashed 3px red';
+});
+
+btWater.addEventListener('click', () => {
+  memeImageContainer.style.border = 'double 5px blue';
+});
+
+meme1.addEventListener('click', (e) => {
+  memeImage.src = e.target.src;
+  memeImage.id = 'meme-image';
+  memeImageContainer.appendChild(memeImage);
+});
+
+meme2.addEventListener('click', (e) => {
+  memeImage.src = e.target.src;
+  memeImage.id = 'meme-image';
+  memeImageContainer.appendChild(memeImage);
+});
+
+meme3.addEventListener('click', (e) => {
+  memeImage.src = e.target.src;
+  memeImage.id = 'meme-image';
+  memeImageContainer.appendChild(memeImage);
+});
+
+meme4.addEventListener('click', (e) => {
+  memeImage.src = e.target.src;
+  memeImage.id = 'meme-image';
+  memeImageContainer.appendChild(memeImage);
 });
